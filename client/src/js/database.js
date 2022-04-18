c90c85d9d569a885e7ed101fa49cb617985c5fc4
Samer Balee <samerbalee@gmail.com>
2022-04-18 18:01:22 +1000

    Fixed undefined value

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,22 +11,7 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (value) => {
-  console.error('getting data from jateDB');
-  // Create a connection to the database
-  const jateDB = await openDB('jate', 1);
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDB.transaction('jate', 'readwrite')
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  
-  }; 
+
   
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, value) => {
@@ -38,11 +23,28 @@ const tx = jateDb.transaction('jate', 'readwrite');
 // open the object store
 const store = tx.objectStore('jate');
 // use the .add() method to pass in content
-const req = store.put({ id: id, value: value })
+const req = store.put({ id: id, value: value });
 // confirm the data was added
 const res = await req;
 console.log('data saved to the jateDB', res);
 } 
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getting data from jateDB');
+  // Create a connection to the database
+  const jateDB = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readonly')
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  
+  }; 
 
 // Start the database.
 initdb();
